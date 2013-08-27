9c50c65be96047b0c4a35b08fabf77270dea5eec
Kevin Yuliawan <kevin.yuliawan@gmail.com>
2013-08-27 18:14:21 -0400

    Fixed popovers on click, now hides them all. But might instead make them contenteditable onclick, and then update accordingly?

--- mycustomscript.js
+++ mycustomscript.js
@@ -5,6 +5,10 @@ $(document).ready(function(){
   var $submit = $('#btn-submit');
   var $clear = $('#btn-clear');
   var $result = $('.result-div');
+  var $test = $('#btn-test');
+  var $chord = $('.chord');
+  var $object = $('.object');
+
 
   function sanitize(markdown){
     // TODO: sanitize input
@@ -37,7 +41,74 @@ $(document).ready(function(){
     $ta.focus();
   });
 
+  $test.click(function(){
+    console.log('test2');
+    console.log(newSong);
+    newSong.updateLyriChord({index:8, lyric:'test',chord:'F#7'});
+    $ta.val(newSong.generateToMarkdown());
+    $result.html(newSong.generateToHTML());
+  });
 
+
+
+  $(document).click(function(){
+    $('.lyric').popover({
+      animation: true,
+      html:'true',
+      selector:'[class="selected"]',
+      placement:'top',
+      trigger: 'manual',
+      content:'<p>Hello</p>',
+    });
+  });
+
+
+  $result.on('click','.lyric',function(){
+    /*
+    1. Setup popover parameters.
+    2. Remove all 'selected' classes then add it to the current one.
+    3. Hide all the popovers that aren't '.selected'
+    4. Show the popover for the current one.
+    */
+    $('.lyric').popover({
+      animation: true,
+      html:'true',
+      selector:'[class="selected"]',
+      placement:'top',
+      trigger: 'manual',
+      content:'<p>Hello</p>',
+    });
+
+
+    $('.lyric').removeClass('selected');
+    $(this).addClass("selected");
+    $('.lyric').not('.selected').popover('hide');
+
+    // $(this).popover('toggle');
+    console.log('lyric: ', $(this));
+    $(this).popover('show');
+  });
+
+  
+
+/*
+  function hideOthers(elem){
+    elem.addClass("selected");
+    $('.lyric').not(".selected").popover('hide');
+    $('.lyric').not(".selected").removeClass("selected");
+  };
+  */
+
+
+
+  $chord.click(function(){
+    console.log('chord click');
+  });
+
+
+
+
+  
 
 
 });
